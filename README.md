22a1738dce9c2e2a20c9de6ff78f1cec6488c39d
CaydendW <cayden@4dewits.com>
2023-04-10 19:23:22 +0200

    Init

---
+++ README.md
@@ -0,0 +1,22 @@
+# Julia render
+Simple little julia render that I used to create a very lovely animation. To see the results of that, see .
+
+# Usage
+1. Edit src/main.c and change the constants as desired. **N.B: CHANGE THE `THREADS` VALUE TO THE AMMOUNT OF CORES YOU HAVE OR SUFFER**. Modify how the colours array is filled in for nicer colours.
+2. run `make`
+3. run `./julia`
+  1. **OPTIONAL**: Run `./julia &` instead to do it in the background instead
+  2. **OPTIONAL**: Run `make renice` to give the process slightly more favourable CPU usage (Might bring your computer to a crawl)
+4. Once the program is done, use `make render` (Or `make nvrender` to use CUDA on an NVidia graphics card) to render a video file. 
+5. **OPTIONAL**: Use `make small` (Or `make nvsmall` to use CUDA on an NVidia graphics card) to compress the video into a more standard video size
+
+# Improvements
+- A possible improvement would be to move the Julia renderer to the GPU using GLSL or CUDA or something but I decided just to hurt my CPU instead. 
+- Something else to improve would be to find a way to avoid using SDL in favour of something a little faster. STBI renderer the PNGs a little too slow so I stuck with SDL instead.
+- Another good idea would be to figure out a format to replace PNG. Compressing the PNGs is costly. The issue is that disk space is limited and PNGs compress the best without losing detail in the image. Maybe making some sort of "on-the-fly" stitching together of the images so that only the video remains but I was too lazy to do this.
+
+# Colours
+The colour palette is taken from https://github.com/HackerPoet/FractalSoundExplorer. It is pre-rendered instead as to make it *slightly* faster.
+
+# License
+GPL-v3.0 :)
---
+++ src/main.c
@@ -0,0 +1,185 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <fcntl.h>
+#include <math.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <termios.h>
+#include <unistd.h>
+
+#define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
+#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
+
+#define STARTING_POS 0
+
+#define MULTIPLIER 0.7885
+
+#define THREADS 12
+
+#define ZOOM 5
+#define THRESHOLD 4
+
+#define LOG2 0.69314718055994528622676398299518041312694549560546875
+#define LOGPOINT5 -0.69314718055994528622676398299518041312694549560546875
+#define TAU 6.28318530717958647692528676655900576839433879875021164
+
+/*#define TEST*/
+
+#ifdef TEST
+
+#define RESX (7680 / 2)
+#define RESY (4320 / 2)
+
+#define COLOUR_DEPTH (size_t)30000
+
+#define ITTERATIONS 2500
+
+#define ROTATE_AMMOUNT 0.01
+
+#else
+
+#define RESX 3840
+#define RESY 2160
+
+#define COLOUR_DEPTH (size_t)65536
+
+#define ITTERATIONS (8192 * 2)
+
+#define ROTATE_AMMOUNT 0.0004363323129985824
+
+#endif
+
+typedef struct tinfo {
+  pthread_t pid;
+  size_t tid;
+  long double re;
+  long double im;
+} tinfo_t;
+
+uint32_t *framebuffer = NULL;
+
+uint32_t colours[COLOUR_DEPTH];
+
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
+SDL_Texture *texture = NULL;
+SDL_Surface *surface = NULL;
+
+static void *render(void *_inf) {
+  tinfo_t *inf = _inf;
+
+  long double real = inf->re;
+  long double imag = inf->im;
+
+  for (size_t i = inf->tid; i < RESY; i += THREADS)
+    for (size_t j = 0; j < RESX; j++) {
+      long double zx = (j - RESX / 2.0) * ZOOM / RESX;
+      long double zy = (i - RESY / 2.0) * ZOOM / RESX;
+
+      size_t k = 0;
+
+      while (k < ITTERATIONS && zx * zx + zy * zy <= THRESHOLD) {
+        long double nx = zx * zx - zy * zy + real;
+        long double ny = 2 * zx * zy + imag;
+
+        zx = nx;
+        zy = ny;
+
+        k++;
+      }
+
+      if (k == ITTERATIONS)
+        framebuffer[i * RESX + j] = 0;
+      else {
+        long double smooth_colour =
+          k + 1 - ((LOGPOINT5 + log(log(zx * zx + zy * zy))) / (LOG2));
+        framebuffer[i * RESX + j] = colours[(
+          size_t)(smooth_colour / (long double)ITTERATIONS * COLOUR_DEPTH)];
+      }
+    }
+
+  pthread_exit(NULL);
+}
+
+static inline void save_img(char *name) {
+  SDL_UpdateTexture(texture, NULL, framebuffer, RESX * sizeof(uint32_t));
+  SDL_RenderReadPixels(renderer, NULL, surface->format->format, surface->pixels,
+                       surface->pitch);
+  IMG_SavePNG(surface, name);
+}
+
+static void sig(int p) {
+  (void)p;
+  exit(0);
+}
+
+int main() {
+  mkdir("./pics", 0755); // drwxr-xr-x
+
+  signal(SIGINT, sig);
+
+  window =
+    SDL_CreateWindow("Julia", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                     RESX, RESY, SDL_WINDOW_HIDDEN);
+
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB888,
+                              SDL_TEXTUREACCESS_TARGET, RESX, RESY);
+  surface = SDL_CreateRGBSurface(0, RESX, RESY, 32, 0, 0, 0, 0);
+
+  SDL_SetRenderTarget(renderer, texture);
+
+  framebuffer = calloc(sizeof(uint32_t), RESX * RESY);
+
+  for (size_t i = 0; i < COLOUR_DEPTH; i++) {
+    long double k = (long double)(i * ITTERATIONS) / (long double)COLOUR_DEPTH;
+    long double n1 = sin(k * 0.1) * 0.5 + 0.5;
+    long double n2 = cos(k * 0.1) * 0.5 + 0.5;
+
+    uint8_t r = n1 * 0xff;
+    uint8_t g = n2 * 0xff;
+    uint8_t b = 1 * 0xff;
+
+    /*colours[i] = (0xff << 24) | (b << 16) | (g << 8) | (r << 0);*/
+    colours[i] = (r << 16) | (g << 8) | (b << 0);
+  }
+
+  char bufname[1024];
+  tinfo_t ts[THREADS] = {0};
+
+  size_t roll = STARTING_POS;
+  long double curr_rotation = ((long double)STARTING_POS * ROTATE_AMMOUNT);
+
+  while (1) {
+    if (curr_rotation >= TAU)
+      exit(0);
+
+    for (size_t i = 0; i < THREADS; i++) {
+      ts[i] = (tinfo_t){
+        .re = MULTIPLIER * cos(curr_rotation),
+        .im = MULTIPLIER * -sin(curr_rotation),
+        .tid = i,
+      };
+
+      pthread_create(&ts[i].pid, NULL, render, &ts[i]);
+    }
+
+    for (size_t i = 0; i < THREADS; i++)
+      pthread_join(ts[i].pid, NULL);
+
+    snprintf(bufname, 1024, "pics/img%07zu.png", roll++);
+    save_img(bufname);
+
+    curr_rotation += ROTATE_AMMOUNT;
+  }
+
+  return 0;
+}
